b7a27d9dc5a973db9311114a243b25f9db6e331e
Anton <imperfection1911@gmail.com>
2018-04-09 15:43:34 +0300

    добавлено кэширование в redis

--- app/handler.py
+++ app/handler.py
@@ -1,6 +1,7 @@
 import re
 from config import Configuration
 from app.cidr import Cidr
+import redis
 
 
 class BotHandler:
@@ -9,6 +10,9 @@ class BotHandler:
         self.config = Configuration()
         self.token = self.config.read_param('telegram', 'token')
         self.help_message = self.config.read_param('telegram', 'help_message')
+        self.cache = redis.StrictRedis(host=self.config.read_param('redis', 'host'),
+                                       port=self.config.read_param('redis', 'port'),
+                                       db=0)
 
     # общий обработчик поступающего текста
     def text_handler(self, message):
@@ -24,38 +28,42 @@ class BotHandler:
             return('я умею только подсети считать.')
 
     # обработчик cidr нотации
-    @staticmethod
-    def cidr_handler(message):
+    def cidr_handler(self, message):
         net, mask = message.split('/')[0], message.split('/')[1]
         if int(mask) not in range(0, 32):
             raise ValueError('маска не входит допустимый диапазон')
-        cidr = Cidr()
-        binary_subnet = cidr.get_subnet(cidr.net_to_bin(net), cidr.mask_to_bin(int(mask)))
-        subnet = cidr.binary_to_ip(binary_subnet)
-        binary_broadcast = cidr.get_broadcast(cidr.net_to_bin(net), cidr.mask_to_bin(int(mask)))
-        broadcast = cidr.binary_to_ip(binary_broadcast)
-        host_min  = cidr.binary_to_ip(cidr.get_host_min(binary_subnet))
-        host_max = cidr.binary_to_ip(cidr.get_host_max(binary_broadcast))
-        host_count = cidr.get_host_count(int(mask))
-        response = "network - {0} \r\n" \
-                   "broadcast - {1} \r\n" \
-                   "hostmin - {2} \r\n" \
-                   "hostmax - {3} \r\n" \
-                   "hosts - {4} \r\n".format(subnet,
-                                             broadcast,
-                                             host_min,
-                                             host_max,
-                                             host_count)
+        response = self.cache.get(message)
+        if response is None:
+            cidr = Cidr()
+            binary_subnet = cidr.get_subnet(cidr.net_to_bin(net), cidr.mask_to_bin(int(mask)))
+            subnet = cidr.binary_to_ip(binary_subnet)
+            binary_broadcast = cidr.get_broadcast(cidr.net_to_bin(net), cidr.mask_to_bin(int(mask)))
+            broadcast = cidr.binary_to_ip(binary_broadcast)
+            host_min  = cidr.binary_to_ip(cidr.get_host_min(binary_subnet))
+            host_max = cidr.binary_to_ip(cidr.get_host_max(binary_broadcast))
+            host_count = cidr.get_host_count(int(mask))
+            response = "network - {0} \r\n " \
+                       "broadcast - {1} \r\n " \
+                       "hostmin - {2} \r\n " \
+                       "hostmax - {3} \r\n " \
+                       "hosts - {4} \r\n".format(subnet,
+                                                 broadcast,
+                                                 host_min,
+                                                 host_max,
+                                                 host_count)
+            self.cache.set(message, response)
         return response
 
     # обработчик диапазона
-    @staticmethod
-    def range_handler(message):
+    def range_handler(self, message):
         hostmin, hostmax = message.split('-')[0].strip(), message.split('-')[1].strip()
-        cidr = Cidr()
-        binary_hostmin = cidr.get_host_min(cidr.net_to_bin(hostmin))
-        binary_hostmax = cidr.get_host_max(cidr.net_to_bin(hostmax))
-        mask = cidr.get_mask_by_range(binary_hostmin, binary_hostmax)
-        network = cidr.binary_to_ip(cidr.get_network_by_range(binary_hostmin, binary_hostmax))
-        response = "{0}/{1}".format(network, mask)
+        response = self.cache.get(message)
+        if response is None:
+            cidr = Cidr()
+            binary_hostmin = cidr.get_host_min(cidr.net_to_bin(hostmin))
+            binary_hostmax = cidr.get_host_max(cidr.net_to_bin(hostmax))
+            mask = cidr.get_mask_by_range(binary_hostmin, binary_hostmax)
+            network = cidr.binary_to_ip(cidr.get_network_by_range(binary_hostmin, binary_hostmax))
+            response = "{0}/{1}".format(network, mask)
+            self.cache.set(message, response)
         return response
